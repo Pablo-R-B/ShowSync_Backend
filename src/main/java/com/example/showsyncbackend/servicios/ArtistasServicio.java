1280cc5f11ff5448bb01966f702d96217a3c33ec
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-03-27 19:35:37 +0100

    Creados dto ArtistasCatalogoDTO

--- src/main/java/com/example/showsyncbackend/servicios/ArtistasServicio.java
+++ src/main/java/com/example/showsyncbackend/servicios/ArtistasServicio.java
@@ -1,9 +1,13 @@
 package com.example.showsyncbackend.servicios;
 
 import lombok.AllArgsConstructor;
+
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 @AllArgsConstructor
 public class ArtistasServicio {
+
 }
