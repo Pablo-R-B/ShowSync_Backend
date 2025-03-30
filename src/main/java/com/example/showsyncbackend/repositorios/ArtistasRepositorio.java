d4655210b084f7a4e4438ba76d90afc38c226f17
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-03-30 18:47:11 +0200

    Creado metodo para listar artistas con sus generos musicales

--- src/main/java/com/example/showsyncbackend/repositorios/ArtistasRepositorio.java
+++ src/main/java/com/example/showsyncbackend/repositorios/ArtistasRepositorio.java
@@ -10,18 +10,8 @@ import java.util.List;
 @Repository
 public interface ArtistasRepositorio extends JpaRepository<Artistas, Integer> {
 
-//    @Query("SELECT a FROM Artistas a JOIN FETCH a.generos")
-//    List<Artistas> findAllWithGeneros();
-
-//    @Query("SELECT DISTINCT a FROM Artistas a LEFT JOIN FETCH a.generos")
-//    List<Object[]> findAllWithGeneros();
-
-//    @Query("SELECT a, g FROM Artistas a JOIN a.generos g")
-//    List<Object[]> findAllWithGeneros();
-
-    @Query("SELECT a.id, a.nombreArtista, g.nombre FROM Artistas a JOIN a.generos g")
+    @Query("SELECT a.id, a.nombreArtista, g.nombre FROM Artistas a JOIN a.generosMusicales g")
     List<Object[]> findAllWithGeneros();
-
 
 
 }
