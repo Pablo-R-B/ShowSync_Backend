0037a2613cfcc9497df6d40a12c11e096b8cf644
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-04-01 21:24:19 +0200

    Creada consulta artistas filtrados por genero

--- src/main/java/com/example/showsyncbackend/repositorios/ArtistasRepositorio.java
+++ src/main/java/com/example/showsyncbackend/repositorios/ArtistasRepositorio.java
@@ -1,8 +1,10 @@
 package com.example.showsyncbackend.repositorios;
 
+import com.example.showsyncbackend.dtos.ArtistasCatalogoDTO;
 import com.example.showsyncbackend.modelos.Artistas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +14,9 @@ public interface ArtistasRepositorio extends JpaRepository<Artistas, Integer> {
 
     @Query("SELECT a.id, a.nombreArtista, a.imagenPerfil,g.nombre FROM Artistas a JOIN a.generosMusicales g")
     List<Object[]> findAllWithGeneros();
+
+    @Query("select a from Artistas a join a.generosMusicales g where g.nombre = :genero")
+    List<Artistas> findArtistasByGenero(@Param("genero") String genero);
 
 
 }
