f1896645d9b7b7a4e0436fad8ff48da9b29cf351
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-04-02 21:33:32 +0200

    Artistas filtrados por genero devuelve artista con su lista de generos

--- src/main/java/com/example/showsyncbackend/servicios/ArtistasServicio.java
+++ src/main/java/com/example/showsyncbackend/servicios/ArtistasServicio.java
@@ -33,47 +33,65 @@ public class ArtistasServicio {
         for (Object[] fila : resultados) {
             String id = String.valueOf(fila[0]);
             String nombreArtista = (String) fila[1];
-            String imagen = (String) fila[2];
-            String nombreGenero = (String) fila[3];
+            String imagenPerfil = (String) fila[2];
+            String generoMusical = (String) fila[3];
 
             // Si el artista ya está en el mapa, agregamos el nuevo género a su lista
             if (!artistasMap.containsKey(id)) {
                 Map<String, Object> artista = new HashMap<>();
                 artista.put("id", id);
-                artista.put("nombreArtista", nombreArtista);
-                artista.put("nombreGenero", new ArrayList<String>());
-                artista.put("imagen", imagen);
+                artista.put("nombre", nombreArtista);
+                artista.put("generos", new ArrayList<String>());
+                artista.put("imagen", imagenPerfil);
                 artistasMap.put(id, artista);
             }
 
             // Añadir el género a la lista de géneros del artista
-            ((List<String>) artistasMap.get(id).get("nombreGenero")).add(nombreGenero);
+            ((List<String>) artistasMap.get(id).get("generos")).add(generoMusical);
         }
 
         return new ArrayList<>(artistasMap.values());
     }
 
     public List<ArtistasCatalogoDTO> artistasPorGenero(String genero) {
+        List<Object[]> resultados = artistasRepositorio.findArtistasByGenero(genero);
+        Map<String, ArtistasCatalogoDTO> artistasMap = new HashMap<>();
 
-        List<Artistas> artistas = artistasRepositorio.findArtistasByGenero(genero);
-        return artistas.stream()
-                .map(this::convertirAArtistasCatalogoDTO)
-                .collect(Collectors.toList());
+        for (Object[] resultado : resultados) {
+            Integer id = (Integer) resultado[0];
+            String nombreArtista = (String) resultado[1];
+            String imagenPerfil = (String) resultado[2];
+            String generoMusical = (String) resultado[3];
+            ArtistasCatalogoDTO dto;
+
+            if (artistasMap.containsKey(nombreArtista)) {
+                dto = artistasMap.get(nombreArtista);
+
+            } else {
+                dto = new ArtistasCatalogoDTO();
+                dto.setNombre(nombreArtista);
+                dto.setImagen(imagenPerfil);
+                dto.setGeneros(new ArrayList<>());
+                artistasMap.put(nombreArtista, dto);
+            }
+            dto.getGeneros().add(generoMusical);
+        }
+        return new ArrayList<>(artistasMap.values());
     }
 
-    private ArtistasCatalogoDTO convertirAArtistasCatalogoDTO(Artistas artista) {
-        ArtistasCatalogoDTO dto = new ArtistasCatalogoDTO();
-        dto.setNombre(artista.getNombreArtista());
-        dto.setImagen(artista.getImagenPerfil());
-
-        List<String> generos = artista.getGenerosMusicales()
-                .stream()
-                .map(GenerosMusicales::getNombre)
-                .collect(Collectors.toList());
-        dto.setGeneros(generos);
-        return dto;
-
-    }
+//    private ArtistasCatalogoDTO convertirAArtistasCatalogoDTO(Artistas artista) {
+//        ArtistasCatalogoDTO dto = new ArtistasCatalogoDTO();
+//        dto.setNombre(artista.getNombreArtista());
+//        dto.setImagen(artista.getImagenPerfil());
+//        List<String> generos = artista.getGenerosMusicales()
+//                .stream()
+//                .map(GenerosMusicales::getNombre)
+//                .collect(Collectors.toList());
+//        System.out.println("Artista: " + artista.getNombreArtista() + ", Géneros: " + generos);
+//        dto.setGeneros(generos);
+//        return dto;
+//
+//    }
 
 
 
