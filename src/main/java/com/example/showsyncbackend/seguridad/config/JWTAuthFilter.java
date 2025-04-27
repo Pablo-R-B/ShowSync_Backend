8905193f3076fde56c6045768b0dd9ef4566de22
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-04-27 11:48:26 +0200

    Merge con ramas Dani, Alba y Lisseth

--- src/main/java/com/example/showsyncbackend/seguridad/config/JWTAuthFilter.java
+++ src/main/java/com/example/showsyncbackend/seguridad/config/JWTAuthFilter.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import io.jsonwebtoken.ExpiredJwtException;
@@ -17,11 +16,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
 
 
 
