78d65c4654946026aafcdc76255e49ed97788b06
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-05-04 18:41:23 +0200

    Modificadas consultas repositorio findAllWithGeneros, findArtistasByGenero y findArtistasByNombre. Creada consulta findByIdWithGeneros

--- src/main/java/com/example/showsyncbackend/repositorios/ArtistasRepositorio.java
+++ src/main/java/com/example/showsyncbackend/repositorios/ArtistasRepositorio.java
@@ -9,31 +9,50 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface ArtistasRepositorio extends JpaRepository<Artistas, Integer> {
+        @Query("""
+        SELECT DISTINCT a
+        FROM Artistas a
+        JOIN FETCH a.generosMusicales g
+        """)
+    Page<Artistas> findAllWithGeneros(Pageable pageable);
 
-    @Query("SELECT a.id, a.nombreArtista, a.imagenPerfil,g.nombre FROM Artistas a JOIN a.generosMusicales g")
-    Page<Object[]> findAllWithGeneros(Pageable pageable);
-
-    @Query("SELECT a.id, a.nombreArtista, a.imagenPerfil, g.nombre " +
-            "FROM Artistas a JOIN a.generosMusicales g " +
-            "WHERE g.nombre = :genero " +
-            "AND (:termino IS NULL OR LOWER(a.nombreArtista) LIKE LOWER(CONCAT('%', :termino, '%')))")
-    Page<Object[]> findArtistasByGenero(
+    @Query("""
+        SELECT DISTINCT a
+        FROM Artistas a
+        JOIN FETCH a.generosMusicales g
+        WHERE g.nombre = :genero
+          AND (:termino IS NULL OR LOWER(a.nombreArtista) LIKE LOWER(CONCAT('%', :termino, '%')))
+        """)
+    Page<Artistas> findArtistasByGenero(
             @Param("genero") String genero,
             @Param("termino") String termino,
             Pageable pageable
     );
 
-    @Query("SELECT a.id, a.nombreArtista, a.imagenPerfil, g.nombre " +
-            "FROM Artistas a JOIN a.generosMusicales g " +
-            "WHERE LOWER(a.nombreArtista) LIKE LOWER(CONCAT('%', :termino, '%'))")
-    Page<Object[]> findArtistasByNombre(
+    @Query("""
+        SELECT DISTINCT a
+        FROM Artistas a
+        JOIN FETCH a.generosMusicales g
+        WHERE LOWER(a.nombreArtista) LIKE LOWER(CONCAT('%', :termino, '%'))
+        """)
+    Page<Artistas> findArtistasByNombre(
             @Param("termino") String termino,
             Pageable pageable
     );
+
+
+    @Query("""
+        SELECT DISTINCT a
+        FROM Artistas a
+        JOIN FETCH a.generosMusicales g
+        WHERE a.id = :id
+        """)
+    Optional<Artistas> findByIdWithGeneros(@Param("id") Integer id);
 
 
 
