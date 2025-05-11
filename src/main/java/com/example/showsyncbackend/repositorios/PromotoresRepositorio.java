d73eea6e955f7cf7e5163c66b97e3421195c563b
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-05-11 13:26:18 +0200

    Método generateToken extrae tambien el id del usuario

--- src/main/java/com/example/showsyncbackend/repositorios/PromotoresRepositorio.java
+++ src/main/java/com/example/showsyncbackend/repositorios/PromotoresRepositorio.java
@@ -1,10 +1,16 @@
 package com.example.showsyncbackend.repositorios;
 
 
+import com.example.showsyncbackend.modelos.Eventos;
 import com.example.showsyncbackend.modelos.Promotores;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PromotoresRepositorio extends JpaRepository<Promotores,Integer> {
+    Optional<Promotores> findByUsuarioId(Integer usuarioId);
+
 }
