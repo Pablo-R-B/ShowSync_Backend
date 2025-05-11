aba0b4907bf01d3e70461e09156af48f961e2376
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-05-11 13:23:52 +0200

    Creado PostulacionEventosRepositorio

---
+++ src/main/java/com/example/showsyncbackend/repositorios/PostulacionEventosRepositorio.java
@@ -0,0 +1,7 @@
+package com.example.showsyncbackend.repositorios;
+
+import com.example.showsyncbackend.modelos.PostulacionEvento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostulacionEventosRepositorio extends JpaRepository<PostulacionEvento, Integer> {
+}
