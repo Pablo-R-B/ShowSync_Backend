6e8805ab7d21516001bf1dfbefe371039a6cddbd
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-05-11 13:28:37 +0200

    Creada consulta buscar promotor por su id de usuario

--- src/main/java/com/example/showsyncbackend/repositorios/PromotoresRepositorio.java
+++ src/main/java/com/example/showsyncbackend/repositorios/PromotoresRepositorio.java
@@ -1,12 +1,10 @@
 package com.example.showsyncbackend.repositorios;
 
 
-import com.example.showsyncbackend.modelos.Eventos;
 import com.example.showsyncbackend.modelos.Promotores;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
