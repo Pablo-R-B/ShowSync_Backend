49722d00636778d0b960384b5ceda006219a3668
alba <sfg@sfg.es>
2025-05-23 21:14:14 +0200

    Cambio a java 22.

--- src/main/java/com/example/showsyncbackend/seguridad/config/SecurityConfig.java
+++ src/main/java/com/example/showsyncbackend/seguridad/config/SecurityConfig.java
@@ -43,6 +43,8 @@ public class SecurityConfig {
                                 "/static/**",
                                 "/promotores/**",
                                 "/eventos/**",
+                                "/artistas/**",
+                                "/genero/**",
                                 "/error"
 
 
