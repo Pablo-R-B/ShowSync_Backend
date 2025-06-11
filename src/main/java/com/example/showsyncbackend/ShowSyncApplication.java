aa7f34ccd9223bf154635f96f6dc6037bfbdfd18
alba <sfg@sfg.es>
2025-06-11 13:39:00 +0200

    MEJORA: service que hace copia de seguridad automatizada de la bbdd.

--- src/main/java/com/example/showsyncbackend/ShowSyncApplication.java
+++ src/main/java/com/example/showsyncbackend/ShowSyncApplication.java
@@ -3,8 +3,10 @@ package com.example.showsyncbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ShowSyncApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShowSyncApplication.class, args);
